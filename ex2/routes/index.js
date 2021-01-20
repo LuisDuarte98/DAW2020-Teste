c05b5997719b71c63a78743901512df61ebd7e87
LuisDuarte98 <36744365+LuisDuarte98@users.noreply.github.com>
2021-01-20 15:17:06 +0000

    another commit =)

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -6,7 +6,7 @@ var axios = require('axios');
 var token = ""
 
 // Obter token no arranque da aplicação ==> Pode conter algum delay =(
-axios.post('http://clav-api.di.uminho.pt/v2//users/login',{username: "pri2020@teste.uminho.pt", password: "123"})
+axios.post('http://clav-api.di.uminho.pt/v2//users/login',{username: "daw2020@teste.uminho.pt", password: "232"})
     .then((res) => {
         console.log('Obtive com sucesso',res.data.token);
         token = res.data.token;
@@ -20,15 +20,45 @@ router.get('/', function(req, res, next) {
     res.render('index');
 });
 
-/* GET classses. */
+/* GET classes. */
 router.get('/classes', function(req, res, next) {
     axios.get('http://clav-api.di.uminho.pt/v2/classes?nivel=1&token=' + token)
         .then(result => {
-            res.render('index', { classes: result.data })
+            res.render('classesIndex', { classes: result.data })
         })
         .catch(error => {
             res.render('error', { error: error })
         });
+});
+
+/* GET classe by id. */
+router.get('/classes/:id', function(req, res, next) {
+    axios.get('http://clav-api.di.uminho.pt/v2/classes/' + req.params.id + '?token=' + token)
+    .then(result => {
+        const nivel = result.data.nivel
+        if (nivel == 3){
+            /*
+            axios.get('http://clav-api.di.uminho.pt/v2/classes/c' + req.params.id + '/procRel' + '?token=' + token)
+                .then(relacionados => {
+                    res.render('classe3', { 
+                        classe: result.data,
+                        relacionados: relacionados.data,
+                    })
+                })
+                .catch(error => {
+                    res.render('error', { error: error })
+                })
+            */
+        }
+        else{
+            res.render('classe', { 
+                classe: result.data,
+             })
+        }      
+    })
+    .catch(error => {
+        res.render('error', { error: error })
+    });
 });
 
 /* GET termos de indices. */
