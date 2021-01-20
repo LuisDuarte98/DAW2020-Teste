2d9b57863b10881cf322443e45161ccb9b6e2106
LuisDuarte98 <36744365+LuisDuarte98@users.noreply.github.com>
2021-01-20 14:58:01 +0000

    begin ex2

--- ex2/routes/index.js
+++ ex2/routes/index.js
@@ -3,10 +3,43 @@ var router = express.Router();
 var axios = require('axios');
 
 
-const apikey = ""
+var token = ""
+
+// Obter token no arranque da aplicação ==> Pode conter algum delay =(
+axios.post('http://clav-api.di.uminho.pt/v2//users/login',{username: "pri2020@teste.uminho.pt", password: "123"})
+    .then((res) => {
+        console.log('Obtive com sucesso',res.data.token);
+        token = res.data.token;
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+/* GET classses. */
+router.get('/classes', function(req, res, next) {
+    axios.get('http://clav-api.di.uminho.pt/v2/classes?nivel=1&token=' + token)
+        .then(result => {
+            res.render('index', { classes: result.data })
+        })
+        .catch(error => {
+            res.render('error', { error: error })
+        });
+});
+
+/* GET termos de indices. */
+router.get('/termos', function(req, res, next) {
+    axios.get('http://clav-api.di.uminho.pt/v2/termosIndice?token=' + token)
+        .then(result => {
+            res.render('termos', { termos: result.data })
+        })
+        .catch(error => {
+            res.render('error', { error: error })
+        });
 });
 
 module.exports = router;
